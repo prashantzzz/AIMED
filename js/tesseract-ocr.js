a5f772fd1730a7754e326800a5a79b3c818752f3
Prashantzzz <prashant2003prashant@gmail.com>
2024-03-18 13:44:02 +0530

    added extract text page

---
+++ js/tesseract-ocr.js
@@ -0,0 +1,110 @@
+$( document ).ready(function() {
+	var inputs = document.querySelectorAll( '.inputfile' );
+	Array.prototype.forEach.call( inputs, function( input )
+	{
+		var label	 = input.nextElementSibling,
+			labelVal = label.innerHTML;
+
+		input.addEventListener( 'change', function( e )
+		{
+			var fileName = '';
+			if( this.files && this.files.length > 1 )
+				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			else
+				fileName = e.target.value.split( '\\' ).pop();
+
+			if( fileName ){
+				label.querySelector( 'span' ).innerHTML = fileName;
+
+				let reader = new FileReader();
+				reader.onload = function () {
+					let dataURL = reader.result;
+					$("#selected-image").attr("src", dataURL);
+				}
+				let file = this.files[0];
+				reader.readAsDataURL(file);
+				startRecognize(file);
+			}
+			else{
+				label.innerHTML = labelVal;
+				
+				$("#log").empty();
+			}
+		});
+
+		// Firefox bug fix
+		input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+		input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+	});
+});
+
+$("#startLink").click(function () {
+	var img = document.getElementById('selected-image');
+	startRecognize(img);
+});
+
+function startRecognize(img){
+	recognizeFile(img);
+}
+
+function progressUpdate(packet){
+	var log = document.getElementById('log');
+
+	if(log.firstChild && log.firstChild.status === packet.status){
+		if('progress' in packet){
+			var progress = log.firstChild.querySelector('progress')
+			progress.value = packet.progress
+		}
+	}else{
+		var line = document.createElement('div');
+		line.status = packet.status;
+		var status = document.createElement('div')
+		status.className = 'status'
+		status.appendChild(document.createTextNode(packet.status))
+		line.appendChild(status)
+
+		if('progress' in packet){
+			var progress = document.createElement('progress')
+			progress.value = packet.progress
+			progress.max = 1
+			line.appendChild(progress)
+		}
+
+
+		if(packet.status == 'done'){
+			log.innerHTML = ''
+			var pre = document.createElement('pre')
+			pre.appendChild(document.createTextNode(packet.data.text.replace(/\n\s*\n/g, '\n')))
+			line.innerHTML = ''
+			line.appendChild(pre)
+			
+		}
+
+		log.insertBefore(line, log.firstChild)
+	}
+}
+
+function recognizeFile(file){
+	$("#log").empty();
+  	const corePath = window.navigator.userAgent.indexOf("Edge") > -1
+    ? 'js/tesseract-core.asm.js'
+    : 'js/tesseract-core.wasm.js';
+
+
+	const worker = new Tesseract.TesseractWorker({
+		corePath,
+	});
+
+	worker.recognize(file,
+		$("#langsel").val()
+	)
+		.progress(function(packet){
+			console.info(packet)
+			progressUpdate(packet)
+
+		})
+		.then(function(data){
+			console.log(data)
+			progressUpdate({ status: 'done', data: data })
+		})
+}
