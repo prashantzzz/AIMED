f257a48d27e10e942472c65c54de5d43552fee55
Prashantzzz <prashant2003prashant@gmail.com>
2024-03-21 16:25:03 +0530

    added generative ai desease prediction page

--- healthcare-chatbot-master/chat_bot.py
+++ healthcare-chatbot-master/chat_bot.py
@@ -1,8 +1,8 @@
-import streamlit as st
+#https://github.com/itachi9604/healthcare-chatbot
 import re
 import pandas as pd
 from sklearn import preprocessing
-from sklearn.tree import DecisionTreeClassifier, _tree
+from sklearn.tree import DecisionTreeClassifier,_tree
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
@@ -11,104 +11,147 @@ import csv
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# Load data
-training = pd.read_csv('Data/Training.csv')
-
-# Preprocess data
-cols = training.columns[:-1]
+training = pd.read_csv('healthcare-chatbot-master/Data/Training.csv')
+testing= pd.read_csv('healthcare-chatbot-master/Data/Testing.csv')
+cols= training.columns
+cols= cols[:-1]
 x = training[cols]
 y = training['prognosis']
+y1= y
+
+reduced_data = training.groupby(training['prognosis']).max()
+
+#mapping strings to numbers
 le = preprocessing.LabelEncoder()
 le.fit(y)
 y = le.transform(y)
 
-# Split data
+
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+testx    = testing[cols]
+testy    = testing['prognosis']  
+testy    = le.transform(testy)
 
-# Train model
-clf = DecisionTreeClassifier()
-clf.fit(x_train, y_train)
 
-# Helper functions
+clf1  = DecisionTreeClassifier()
+clf = clf1.fit(x_train,y_train)
+# print(clf.score(x_train,y_train))
+# print ("cross result========")
+scores = cross_val_score(clf, x_test, y_test, cv=3)
+# print (scores)
+# print (scores.mean())
+
+
+model=SVC()
+model.fit(x_train,y_train)
+# print("for svm: ")
+# print(model.score(x_test,y_test))
+
+importances = clf.feature_importances_
+indices = np.argsort(importances)[::-1]
+features = cols
+
+severityDictionary=dict()
+description_list = dict()
+precautionDictionary=dict()
+food_avoid=dict()
+
+symptoms_dict = {}
+
+for index, symptom in enumerate(x):
+       symptoms_dict[symptom] = index
+def calc_condition(exp,days):
+    sum=0
+    for item in exp:
+         sum=sum+severityDictionary[item] #severity based on the days and symptoms
+    if((sum*days)/(len(exp)+1)>13): #severe
+        print("You should take the consultation from doctor. ")
+    else:  #Not severe
+        print("It might not be that bad but you should take precautions.")
+
+
 def getDescription():
-    description_list = {}
-    with open('MasterData/symptom_Description.csv') as csv_file:
+    global description_list
+    with open('healthcare-chatbot-master/MasterData/symptom_Description.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
         for row in csv_reader:
-            description_list[row[0]] = row[1]
-    return description_list
+            _description={row[0]:row[1]}
+            description_list.update(_description)
+
 
 def getSeverityDict():
-    severityDictionary = {}
-    with open('MasterData/symptom_severity.csv') as csv_file:
+    global severityDictionary
+    with open('healthcare-chatbot-master/MasterData/symptom_severity.csv') as csv_file:
+
         csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            severityDictionary[row[0]] = int(row[1])
-    return severityDictionary
+        line_count = 0
+        try:
+            for row in csv_reader:
+                _diction={row[0]:int(row[1])}
+                severityDictionary.update(_diction)
+        except:
+            pass
+
 
 def getprecautionDict():
-    precautionDictionary = {}
-    with open('MasterData/symptom_precaution.csv') as csv_file:
+    global precautionDictionary
+    with open('healthcare-chatbot-master/MasterData/symptom_precaution.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
         for row in csv_reader:
-            precautionDictionary[row[0]] = [row[1], row[2], row[3], row[4]]
-    return precautionDictionary
+            _prec={row[0]:[row[1],row[2],row[3],row[4]]}
+            precautionDictionary.update(_prec)
 
 def getfood_avoid():
-    food_avoid = {}
-    with open('MasterData/Disease_avoid_food.csv', mode='r', newline='') as csv_file:
+    global food_avoid
+    with open('healthcare-chatbot-master/MasterData/Disease_avoid_food.csv', mode='r', newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
         for row in csv_reader:
-            try:
-                food_avoid[row[0]] = [row[1], row[2], row[3], row[4]]
-            except:
-                try:
-                    food_avoid[row[0]] = [row[1], row[2], row[3]]
-                except:
-                    food_avoid[row[0]] = [row[1], row[2]]
-    return food_avoid
+            try: _prec={row[0]:[row[1],row[2],row[3],row[4]]}
+            except: 
+                try: _prec={row[0]:[row[1],row[2],row[3]]}
+                except: _prec={row[0]:[row[1],row[2]]}
+            food_avoid.update(_prec)
 
-description_list = getDescription()
-severityDictionary = getSeverityDict()
-precautionDictionary = getprecautionDict()
-food_avoid = getfood_avoid()
+def getInfo():
+    print("----------------AIMED Disease Predictor----------------------")
+    print("\nYour Name: ",end="")
+    name=input("")
+    print("Hello, ",name)
 
-def check_pattern(dis_list, inp):
+def check_pattern(dis_list,inp):
     pred_list=[]
-    inp = inp.replace(' ', '_')
+    inp=inp.replace(' ','_')
     patt = f"{inp}"
     regexp = re.compile(patt)
-    pred_list = [item for item in dis_list if regexp.search(item)]
-    if len(pred_list) > 0:
-        return 1, pred_list
+    pred_list=[item for item in dis_list if regexp.search(item)]
+    if(len(pred_list)>0):
+        return 1,pred_list
     else:
-        return 0, []
-
+        return 0,[]
 def sec_predict(symptoms_exp):
+    df = pd.read_csv('Data/Training.csv')
+    X = df.iloc[:, :-1]
+    y = df['prognosis']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20)
     rf_clf = DecisionTreeClassifier()
-    rf_clf.fit(x_train, y_train)
+    rf_clf.fit(X_train, y_train)
 
-    symptoms_dict = {symptom: index for index, symptom in enumerate(x)}
+    symptoms_dict = {symptom: index for index, symptom in enumerate(X)}
     input_vector = np.zeros(len(symptoms_dict))
     for item in symptoms_exp:
-        input_vector[[symptoms_dict[item]]] = 1
+      input_vector[[symptoms_dict[item]]] = 1
 
     return rf_clf.predict([input_vector])
 
-def calc_condition(exp, days):
-    sum = 0
-    for item in exp:
-        sum = sum + severityDictionary.get(item, 0)  # severity based on the days and symptoms
-    if (sum * days) / (len(exp) + 1) > 13:  # severe
-        st.write("You should take the consultation from doctor. ")
-    else:  # Not severe
-        st.write("It might not be that bad but you should take precautions.")
 
 def print_disease(node):
     node = node[0]
-    val = node.nonzero()
+    val  = node.nonzero() 
     disease = le.inverse_transform(val[0])
-    return list(map(lambda x: x.strip(), list(disease)))
+    return list(map(lambda x:x.strip(),list(disease)))
 
 def tree_to_code(tree, feature_names):
     tree_ = tree.tree_
@@ -117,20 +160,38 @@ def tree_to_code(tree, feature_names):
         for i in tree_.feature
     ]
 
-    chk_dis = ",".join(feature_names).split(",")
+    chk_dis=",".join(feature_names).split(",")
     symptoms_present = []
-    disease_input = st.text_input("Enter the symptom you are experiencing:")
-    conf, cnf_dis = check_pattern(chk_dis, disease_input)
-    if conf == 1:
-        if len(cnf_dis) > 1:
-            conf_inp = st.selectbox(f"Select the one you meant (options: {cnf_dis})", cnf_dis)
-            disease_input = conf_inp
+    print("\nEnter the symptom you are experiencing: ",end="")
+
+    while True:
+        disease_input = input("").replace(" ","_").lower()
+        conf,cnf_dis=check_pattern(chk_dis,disease_input)
+        if conf==1:
+            print("searches related to input: ")
+            for num,it in enumerate(cnf_dis):
+                print(num,")",it.replace("_"," ").capitalize())
+            if num!=0:
+                conf_inp = int(input(f"Select the one you meant (option 0 - {num}): "))
+            else:
+                conf_inp=0
+
+            disease_input=cnf_dis[conf_inp]
+            break
+            # print("Did you mean: ",cnf_dis,"?(yes/no) :",end="")
+            # conf_inp = input("")
+            # if(conf_inp=="yes"):
+            #     break
         else:
-            disease_input = cnf_dis[0]
-
-    num_days = st.number_input(f"From how many days are you having {disease_input}:", min_value=0)
-
+            print("Enter any other symptom: ")
+    while(True):
+        try: 
+            num_days=int(input(f"Okay, from how many days are you having {disease_input}: "))
+            break
+        except: print("Enter valid value")
+    
     def recurse(node, depth):
+        indent = "  " * depth
         if tree_.feature[node] != _tree.TREE_UNDEFINED:
             name = feature_name[node]
             threshold = tree_.threshold[node]
@@ -139,46 +200,64 @@ def tree_to_code(tree, feature_names):
                 val = 1
             else:
                 val = 0
-            if val <= threshold:
-                recurse(tree_.children_left[node], depth        + 1)
+            if  val <= threshold:
+                recurse(tree_.children_left[node], depth + 1)
             else:
                 symptoms_present.append(name)
                 recurse(tree_.children_right[node], depth + 1)
         else:
             present_disease = print_disease(tree_.value[node])
+            # print( "You may have" +  present_disease )
             red_cols = reduced_data.columns 
             symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero()]
-            st.write("Are you experiencing any ")
+            # dis_list=list(symptoms_present)
+            # if len(dis_list)!=0:
+            #     print("symptoms present  " + str(list(symptoms_present)))
+            # print("symptoms given "  +  str(list(symptoms_given)) )
+            print("Are you experiencing any ")
             symptoms_exp=[]
             for syms in list(symptoms_given):
                 inp=""
-                st.write(f"{syms}?")
-                inp = st.radio(f"{syms.capitalize()}?", options=["Yes", "No"]).lower()
-                if inp == "yes":
+                print(syms,"? : ",end='')
+                while True:
+                    inp=input("").lower()
+                    if(inp=="yes" or inp=="no"):
+                        break
+                    else:
+                        print("provide proper answers i.e. (yes/no) : ",end="")
+                if(inp=="yes"):
                     symptoms_exp.append(syms)
 
-            second_prediction = sec_predict(symptoms_exp)
-            calc_condition(symptoms_exp, num_days)
-            if present_disease[0] == second_prediction[0]:
-                st.write(f"You may have {present_disease[0]}")
-                st.write(description_list[present_disease[0]])
+            second_prediction=sec_predict(symptoms_exp)
+            # print(second_prediction)
+            calc_condition(symptoms_exp,num_days)
+            if(present_disease[0]==second_prediction[0]):
+                print("You may have", present_disease[0])
+                print("\t"+description_list[present_disease[0]])
+
             else:
-                st.write(f"You may have {present_disease[0]} or {second_prediction[0]}")
-                st.write(description_list[present_disease[0]])
-                st.write(description_list[second_prediction[0]])
+                print("You may have", present_disease[0], "or ", second_prediction[0])
+                print("\t"+description_list[present_disease[0]])
+                print("\t"+description_list[second_prediction[0]])
 
-            precaution_list = precautionDictionary[present_disease[0]]
-            st.write("Take following measures:")
-            for i, j in enumerate(precaution_list):
-                st.write(f"{i+1}) {j.capitalize()}")
+            # print(description_list[present_disease[0]])
+            precution_list=precautionDictionary[present_disease[0]]
+            print("\nTake following measures : ")
+            for  i,j in enumerate(precution_list):
+                print(f"{i+1})",j.capitalize()) 
 
-            st.write(f"Avoid these food items for {present_disease[0]}:")
-            for i, j in enumerate(food_avoid[present_disease[0]]):
-                st.write(f"{i+1}) {j.capitalize()}")
+            # confidence_level = (1.0*len(symptoms_present))/len(symptoms_given)
+            # print("(confidence level is " + str(confidence_level))
+            
+            print(f"Avoid these food items for {present_disease[0]}:")
+            for i,j in enumerate(food_avoid[present_disease[0]]):
+                print(f"{i+1})",j.capitalize())
 
     recurse(0, 1)
-
-if __name__ == "__main__":
-    st.title("Disease Predictor")
-    tree_to_code(clf, cols)
-
+getSeverityDict()
+getDescription()
+getfood_avoid()
+getprecautionDict()
+getInfo()
+tree_to_code(clf,cols)
+print("------------------------------------------------------------")
